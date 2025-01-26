6758c1acc6ae907fe0e764a4e08a22a90b587c80
n0neill <nsoneill3@gmail.com>
2025-01-26 01:33:11 +0000

    icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+    import { Sidebar, SidebarBrand, SidebarCta, SidebarDropdownItem, SidebarDropdownWrapper, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import { ChartPieSolid, GridSolid, CartSolid, MailBoxSolid, UserSolid, ArrowRightToBracketOutline, EditOutline } from 'flowbite-svelte-icons';
+	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+
 </script>
+
+<Sidebar>
+	<SidebarWrapper>
+	  <SidebarGroup>
+		<SidebarDropdownWrapper label="Defender">
+			<svelte:fragment slot="icon">
+			  <CartSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+			</svelte:fragment>
+			<SidebarDropdownItem label="Advanced hunting" />
+			<SidebarDropdownItem label="Email details" />
+			<SidebarDropdownItem label="Export" />
+		  </SidebarDropdownWrapper>
+
+		<SidebarDropdownWrapper label="Zscaler">
+		  <svelte:fragment slot="icon">
+			<CartSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+		  </svelte:fragment>
+		  <SidebarDropdownItem label="URL Lookup" />
+		  <SidebarDropdownItem label="URL Category Update" />
+		  <SidebarDropdownItem label="Export" />
+		</SidebarDropdownWrapper>
+	  </SidebarGroup>
+	</SidebarWrapper>
+</Sidebar>
 
 {@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite' // [!code ++] // [!code focus]
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		Icons({ // [!code ++] // [!code focus]
+			compiler: 'svelte', // [!code ++] // [!code focus]
+		}), // [!code ++] // [!code focus]
+	]
 });
